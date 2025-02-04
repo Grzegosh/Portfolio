c417efb4a704492b89f7ba6e284ec46baaa8c123
Grzegosh <grzegorznaporowski1@gmail.com>
2025-02-04 17:56:23 +0100

    Fixed merging problem

--- Air_Pollution/src/utils/data_fetcher.py
+++ Air_Pollution/src/utils/data_fetcher.py
@@ -1,5 +1,6 @@
 from Air_Pollution.src.utils.configuration import Configuration
 from openaq import OpenAQ
+import json
 import ast
 
 class DataFetcher:
@@ -23,6 +24,34 @@ class DataFetcher:
             return client
         else:
             raise TypeError("Client is not a OpenAQ class")
+
+    def fetch_locations(self, save=True):
+        """
+        Function that save json file with location id
+        :param save: If True the file will be saved, if False, the function will return the dictionary itself.
+        :return:
+        JSON file with locations id or python dictionary.
+        """
+        location_dictionary = {}
+        client = self.connect_to_client()
+        results = client.locations.list(
+            limit=1000,
+            bbox=self.coords
+        ).results
+
+        for location in results:
+            location_dictionary[location.id] = location.name
+
+        if save:
+            with open("locations.json", "w") as loc:
+                json.dump(
+                    location_dictionary,
+                    loc,
+                    sort_keys=True,
+                    ensure_ascii=False
+                )
+        else:
+            return location_dictionary
 
 
 
