76ac1c51c88d0f2f95d604ed22b32e552bb3a9f2
Grzegosh <grzegorznaporowski1@gmail.com>
2025-02-09 18:48:33 +0100

    switched to sql alchemy

--- Air_Pollution/src/utils/sql_utils/connect_to_db.py
+++ Air_Pollution/src/utils/sql_utils/connect_to_db.py
@@ -1,6 +1,6 @@
 import os.path
-
-import mysql.connector
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
 from Air_Pollution.src.utils.configuration import  Configuration
 from Air_Pollution.src.utils.data_fetcher import DataFetcher
 
@@ -15,49 +15,77 @@ class SQLManagement:
         self.password = self.config.get("MySQL", "password")
 
 
-    def connect_to_sql(self):
-        db_config = {
-            'host':self.host,
-            'port':self.port,
-            'user':self.user,
-            'password':self.password
-        }
+
+    def connect_to_sql(self,database=None):
         try:
-            conn = mysql.connector.connect(**db_config)
-            return conn
-        except mysql.connector.Error as err:
-            print(f"Error: {err}")
+            if database is None:
+                engine = create_engine(f"mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}")
+                engine.connect()
+                print("Connection estabilised succesfully.")
+                return engine
+            else:
+                engine = create_engine(f"mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{database}")
+                print(f"Connection estabilised succesfully for database: {database}")
+                return engine
+        except SQLAlchemyError as s:
+            raise RuntimeError(f"No connection was estabilished: {s}")
+
 
 
     def create_schema(self):
         connection = self.connect_to_sql()
-        cursor = connection.cursor()
         file_path = os.path.join(os.getcwd(), "create_schema.sql")
-        with open(file_path, "r") as sql:
-            cursor.execute(sql.read())
-            sql.close()
-            cursor.close()
-        print(f"Schema has been succesfully created.")
+        try:
+            with open (file_path, "r") as sql_file:
+                sql_statements = sql_file.read()
 
-    def create_table(self, schema:str = 'air_pollution'):
+            with connection.connect() as conn:
+                conn.execute(text(sql_statements))
+                conn.commit()
+
+            print(f"File: {file_path} was executed succesfully.")
+        except Exception as e:
+            raise RuntimeError(f"There was some error connected with executing the commands: {e}")
+
+    def create_table(self, database_name:str = "air_pollution"):
         connection = self.connect_to_sql()
-        cursor = connection.cursor()
         file_path = os.path.join(os.getcwd(), "create_table.sql")
         try:
-            cursor.execute(f"USE {schema};")
-        except mysql.connector.Error:
-            raise Exception(f"Schema {schema} probably does not exists.")
+            with open(file_path, "r") as sql_file:
+                sql_statements = sql_file.read()
+
+            with connection.connect() as conn:
+                conn.execute(text(f"USE {database_name};"))
+                conn.execute(text(sql_statements))
+                conn.commit()
+
+            print(f"File: {file_path} was executed succesfully.")
+
+        except Exception as e:
+            raise RuntimeError(f"There was some error connected with executing the commands: {e}")
+
+
+    def save_data_to_sql(self, save=True,table_name:str = "raw_data"):
         try:
-            with open(file_path, "r") as sql:
-                cursor.execute(sql.read())
-                sql.close()
-                cursor.close()
-        except mysql.connector.Error as err:
-            print(f"Error: {err}")
+            if save:
+                engine = self.connect_to_sql(database="123")
+                data_fetcher = DataFetcher()
+                data = data_fetcher.get_data()
+                data.to_sql(name=table_name, con=engine, index=False, if_exists="append")
+            else:
+                pass
+        except SQLAlchemyError as s:
+            raise Exception(f"There's some problems with connection. {s}")
+
+
+
+
 
 
 test = SQLManagement()
-test.create_table()
+test.save_data_to_sql()
+
+
 
 
 
--- Air_Pollution/src/utils/sql_utils/create_table.sql
+++ Air_Pollution/src/utils/sql_utils/create_table.sql
@@ -8,5 +8,6 @@ lon DECIMAL(9,6),
 o3 FLOAT,
 pm10 FLOAT,
 pm25 FLOAT,
-so2 FLOAT
+so2 FLOAT,
+load_date INT
 );
