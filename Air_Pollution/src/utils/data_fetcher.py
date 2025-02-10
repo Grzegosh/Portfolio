c5c5dc8cfe7368d0a7b9dde552f5bbd61d1e5d23
Grzegosh <grzegorznaporowski1@gmail.com>
2025-02-10 12:11:24 +0100

    Added the possibility to save the data to MySQL tables, and read from them.

--- Air_Pollution/src/utils/data_fetcher.py
+++ Air_Pollution/src/utils/data_fetcher.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import datetime
 from Air_Pollution.src.utils.configuration import Configuration
 from openaq import OpenAQ
 import json
@@ -57,12 +58,10 @@ class DataFetcher:
         else:
             return location_dictionary
 
-
-    def get_data(self, month: str = 1, year: str = 2025):
+    def get_data(self, month: str = "01", year: str = "2025"):
         """
-        Function that saves the data for all the locations id in the locations.json file
-        :return:
-        Union DataFrame with all the data.
+        Function that retrieves data for all location IDs in the locations.json file
+        and returns a DataFrame.
         """
         file_path = os.path.join(os.getcwd(), "locations.json")
         if os.path.exists(file_path):
@@ -72,42 +71,35 @@ class DataFetcher:
             locs = self.fetch_locations(save=False)
 
         keys = locs.keys()
-
-        all_data = pd.DataFrame()
         fs = s3fs.S3FileSystem(anon=True)
+        dataframes = []
 
         for key in keys:
-            file_list = fs.glob(f'openaq-data-archive/records/csv.gz/locationid={key}/year={year}/month={month}/*.csv.gz')
+            file_list = fs.glob(
+                f'openaq-data-archive/records/csv.gz/locationid={key}/year={year}/month={month}/*.csv.gz')
 
-            dataframes = [pd.read_csv(fs.open(file),encoding="utf-8", compression="gzip") for file in file_list]
-            all_data = pd.concat([all_data] + dataframes, ignore_index=True)
+            if not file_list:
+                print(f"Brak plików dla locationid={key}, year={year}, month={month}")
 
-        return all_data.to_csv("pollution_data.csv", encoding="utf-8")
+            for file in file_list:
+                df = pd.read_csv(fs.open(file), encoding="utf-8", compression="gzip")
+                dataframes.append(df)
 
+        if dataframes:
+            concatenated_data = pd.concat(dataframes, ignore_index=True)
+            all_data = concatenated_data[concatenated_data ['parameter'].isin(['pm10','pm25', 'o3', 'so2'])]
+            all_data = all_data.pivot_table(
+                index=['location_id', 'sensors_id', 'location', 'datetime','lat','lon'],
+                columns='parameter',
+                values='value'
+            ).reset_index().fillna(0)
+            now = datetime.datetime.now().strftime("%Y%m%d")
+            all_data['load_date'] = now
+        else:
+            all_data = pd.DataFrame()
 
-    def clean_data(self)->pd.DataFrame:
-        """
-        Function that cleans the data gathered from the "get_data" method
-        :return:
-        DataFrame ready for visualisation.
-        """
-        file_path = os.path.join(os.getcwd(), "pollution_data.csv")
-        try:
-            data = pd.read_csv(file_path)
-        except FileNotFoundError:
-            print(f"File: {file_path} does not exists.")
-            return pd.DataFrame()
-
-        data = data.drop("Unnamed: 0", axis=1)
-        data = data[data['parameter'].isin(['pm10','pm25', 'o3', 'so2'])]
-        pd.set_option("display.max_columns", None)
-        pivot_data = data.pivot_table(
-            index=['location_id', 'sensors_id', 'location', 'datetime','lat','lon'],
-            columns='parameter',
-            values='value'
-        ).reset_index().fillna(0)
-        return pivot_data
-
+        print(f"Number of rows: {len(all_data)}")
+        return all_data
 
 
 
--- Air_Pollution/src/utils/sql_utils/connect_to_db.py
+++ Air_Pollution/src/utils/sql_utils/connect_to_db.py
@@ -1,4 +1,7 @@
 import os.path
+
+import pandas as pd
+import sqlalchemy.exc
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 from Air_Pollution.src.utils.configuration import  Configuration
@@ -20,19 +23,24 @@ class SQLManagement:
         try:
             if database is None:
                 engine = create_engine(f"mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}")
-                engine.connect()
-                print("Connection estabilised succesfully.")
-                return engine
+
             else:
                 engine = create_engine(f"mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{database}")
-                print(f"Connection estabilised succesfully for database: {database}")
-                return engine
+                with engine.connect() as conn:
+                    conn.execute(text(f"USE {database}"))
+                    print(f"Connection to the {database} is estabilished.")
+            return engine
         except SQLAlchemyError as s:
             raise RuntimeError(f"No connection was estabilished: {s}")
 
 
 
     def create_schema(self):
+        """
+        Function that reades the "create_schema.sql" file and executes the command within it.
+        :return:
+        Schema within the MySQL server.
+        """
         connection = self.connect_to_sql()
         file_path = os.path.join(os.getcwd(), "create_schema.sql")
         try:
@@ -48,6 +56,12 @@ class SQLManagement:
             raise RuntimeError(f"There was some error connected with executing the commands: {e}")
 
     def create_table(self, database_name:str = "air_pollution"):
+        """
+        Function that reades the "create_table.sql" file and executes the command within it.
+        :param database_name:
+        :return:
+        Empty table within "database_name".
+        """
         connection = self.connect_to_sql()
         file_path = os.path.join(os.getcwd(), "create_table.sql")
         try:
@@ -65,10 +79,17 @@ class SQLManagement:
             raise RuntimeError(f"There was some error connected with executing the commands: {e}")
 
 
-    def save_data_to_sql(self, save=True,table_name:str = "raw_data"):
+    def save_data_to_sql(self, save:bool=True,table_name:str = "raw_data"):
+        """
+        Function that saves the data into MySQL server.
+        :param save: Indicates if we want to save the data within the "table_name" table.
+        :param table_name: Name of the table in which we want to save the data.
+        :return:
+        Loaded data within the "table_name" table.
+        """
         try:
             if save:
-                engine = self.connect_to_sql(database="123")
+                engine = self.connect_to_sql(database="air_pollution")
                 data_fetcher = DataFetcher()
                 data = data_fetcher.get_data()
                 data.to_sql(name=table_name, con=engine, index=False, if_exists="append")
@@ -78,12 +99,22 @@ class SQLManagement:
             raise Exception(f"There's some problems with connection. {s}")
 
 
+    def read_data_from_sql(self)->pd.DataFrame:
+        """
+        Function that reades the "read_table.sql" and returns it as pandas DataFrame.
+        :return:
+        DataFrame with all the data within the MySQL table.
+        """
+        try:
+            engine = self.connect_to_sql(database="air_pollution")
+            with open("read_table.sql","r") as sql_file:
+                command = sql_file.read()
+                df = pd.read_sql(command, con=engine)
+            return df
+        except sqlalchemy.exc.ProgrammingError as pe:
+            raise Exception(f"You've enetered wrong name of the table. {pe}")
 
 
-
-
-test = SQLManagement()
-test.save_data_to_sql()
 
 
 
