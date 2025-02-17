7a494aa93f634daa1f5fc1710817dcac6bd86ea0
Grzegosh <grzegorznaporowski1@gmail.com>
2025-02-17 19:53:37 +0100

    Added possibility to create choropleth map

---
+++ Air_Pollution/src/utils/geojson.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("/Users/grzegorznaporowski/Desktop/Portfolio")
+
+import pandas as pd
+import requests
+import json
+from air_pollution.src.utils.configuration import Configuration
+
+class GetGeo:
+    def __init__(self):
+        self.config = Configuration()
+        self.url = self.config.get("GeoJson", "url")
+
+
+    def get_geo_data(self):
+        response = requests.get(self.url)
+        if response.status_code != 200:
+            raise ConnectionError("There's some probem with connection to your link.")
+        else:
+            geojson = response.json()
+            return geojson
