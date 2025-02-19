4faa84ed51dde2211fd35ddd352514b528027a0d
Grzegosh <grzegorznaporowski1@gmail.com>
2025-02-19 18:33:37 +0100

    Added possibility to filter choropleth maps

--- Air_Pollution/src/main/main.py
+++ Air_Pollution/src/main/main.py
@@ -12,20 +12,19 @@ from air_pollution.src.utils.sql_utils.connect_to_db import SQLManagement
 from air_pollution.src.utils.streamlit_utils.choropleth_map import Choropleth
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
+from sklearn.preprocessing import MinMaxScaler
 
 #Creating necessary objects
 
 BACKGROUND = Background()
-SIDEBAR = Sidebar(msg="Navigation Panel",options=("About", "Visualisation", "Statistics"))
+SIDEBAR = Sidebar(msg="Navigation Panel",options=("About", "Maps", "Statistics"))
 DATA = SQLManagement()
 CHORO = Choropleth()
+SCALER = MinMaxScaler()
 
 
 #Creating background and Sidebar
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
-st.markdown(BACKGROUND.use_css(), unsafe_allow_html=True)
 side = SIDEBAR.create_sidebar()
 
 #Displaying different things within different sidebar options
@@ -36,18 +35,84 @@ if side == "About":
     with open(path, "r") as about:
         st.write(about.read())
 
-if side == "Visualisation":
+if side == "Maps":
+    pollutants = ['o3', 'pm10', 'pm25', 'so2']
     df = DATA.read_data_from_sql(mode="view")
-    groupped_data = df.groupby('voivodeship')['pm10'].mean().reset_index()
+
+    min_date = df['datetime'].min()
+    max_date = df['datetime'].max()
+
     col1, col2 = st.columns(2)
     with col1:
-        CHORO.create_choropleth_map(
+        RANGE_ = st.slider("Select a date range: ", value=(min_date, max_date), min_value=min_date, max_value=max_date)
+    with col2:
+        AGG_OPTION = st.selectbox("What aggregation you want to perform?",("Mean", "Median"))
+
+    if AGG_OPTION == "Mean":
+        agg_dict = {pollutant: 'mean' for pollutant in pollutants}
+        df = df[(df['datetime']>=RANGE_[0])&(df['datetime']<=RANGE_[1])]
+        if len(df) == 0:
+            st.error("Pick a greater timerange! It seem's like we don't have data for that timerange.")
+
+        groupped_data = df.groupby('voivodeship').agg(agg_dict).reset_index()
+        groupped_data['score'] = groupped_data[pollutants].mean(axis=1)
+    else:
+        agg_dict = {
+            pollutant: lambda x: x[x != 0].median() if x[x != 0].count() > 0 else 0
+            for pollutant in pollutants
+        }
+        groupped_data = df.groupby('voivodeship').agg(agg_dict).reset_index()
+        groupped_data['score'] = groupped_data[pollutants].mean(axis=1)
+
+
+
+    col3, col4 = st.columns(2)
+
+    with col3:
+        fig = CHORO.create_choropleth_map(
             data_frame=groupped_data,
-            loc="voivodeship",
-            feat_key="properties.nazwa",
-            color="pm10"
+            loc='voivodeship',
+            feat_key='properties.nazwa',
+            color='o3',
+            title=f"{AGG_OPTION} value of o3 between {RANGE_[0]} and {RANGE_[1]}"
         )
 
+    with col4:
+        fig = CHORO.create_choropleth_map(
+            data_frame=groupped_data,
+            loc='voivodeship',
+            feat_key='properties.nazwa',
+            color='so2',
+            title=f"{AGG_OPTION} value of so2 between {RANGE_[0]} and {RANGE_[1]}"
+        )
+
+    col5, col6 = st.columns(2)
+
+    with col5:
+        fig = CHORO.create_choropleth_map(
+            data_frame=groupped_data,
+            loc='voivodeship',
+            feat_key='properties.nazwa',
+            color='pm10',
+            title=f"{AGG_OPTION} value of pm10 between {RANGE_[0]} and {RANGE_[1]}"
+        )
+
+    with col6:
+        fig = CHORO.create_choropleth_map(
+            data_frame=groupped_data,
+            loc='voivodeship',
+            feat_key='properties.nazwa',
+            color='pm25',
+            title=f"{AGG_OPTION} value of pm25 between {RANGE_[0]} and {RANGE_[1]}"
+        )
+
+    CHORO.create_choropleth_map(
+        data_frame=groupped_data,
+        loc='voivodeship',
+        feat_key='properties.nazwa',
+        color='score',
+        title=f"General score of air pollution in Poland based on {AGG_OPTION} metric. (Higher score means greater pollution.)"
+    )
 
 
 
--- Air_Pollution/src/utils/streamlit_utils/choropleth_map.py
+++ Air_Pollution/src/utils/streamlit_utils/choropleth_map.py
@@ -11,13 +11,14 @@ class Choropleth:
         self.geojson = GetGeo().get_geo_data()
 
 
-    def create_choropleth_map(self, data_frame, loc, feat_key, color):
+    def create_choropleth_map(self, data_frame, loc, feat_key, color, title=None):
         """
         Function that creates a choropleth map based on a particular dataframe.
         :param data_frame: DataFrame from which we want to create a choropleth map.
         :param loc: DataFrame column name that contains the exact same names that properties key in geojson dict.
         :param feat_key: Path to field in GeoJSON feature object with which to match the values
         :param color: Either a name of a column in `data_frame`, or a pandas Series or
+        :param title: Title of the map.
         array_like object.
         Plotly choropleth map.
         """
@@ -27,12 +28,18 @@ class Choropleth:
             locations=loc,
             featureidkey=feat_key,
             color = color,
-            color_continuous_scale="Viridis",
+            color_continuous_scale="RdYlGn_r",
             fitbounds="locations",
             basemap_visible=False,
+            title=title
         )
         fig.update_layout(
         paper_bgcolor='rgba(0,0,0,0)',
+            geo=dict(
+                bgcolor='rgba(0,0,0,0)'
+            )
+
         )
+
 
         return st.plotly_chart(fig, use_container_width=True)
